dc4ed2d08277b034ee3fddb01ba308e66f0ba6b3
wangjc <wangjaingchao@sinosoft.com.cn>
2019-12-27 15:19:37 +0800

    线程池

---
+++ springboot-threadpool/src/main/java/com/example/config/ExecutorConfig.java
@@ -0,0 +1,17 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @Author 王江超
+ * @Date 2019/12/27 15:17
+ * @Description:
+ */
+@Configuration
+@EnableAsync
+public class ExecutorConfig {
+
+
+
+}
