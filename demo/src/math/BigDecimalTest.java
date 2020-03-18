8d542343b1d82ef746158f9c2ecb125883197a76
wangjc <l>
2020-03-18 21:52:39 +0800

    BigDecimal使用

--- demo/src/math/BigDecimalTest.java
+++ demo/src/math/BigDecimalTest.java
@@ -65,12 +65,8 @@ public class BigDecimalTest {
         System.out.println("位数"+scale);
         System.out.println("最大值"+bigDecimal);
         //设置小数点保留位数  和取舍
-        max.setScale(2,1);
-        String s = String.valueOf(max);
-        String[] split = s.split(".");
-        if (split[1].length() >4){
-            System.out.println("折标比例保留到小数点后4位");
-        }
+        bigDecimal.setScale(2,1);
+
 
         return add;
     }
